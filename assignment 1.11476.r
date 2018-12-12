dafa2d8cb597fcc088796c8c69922bb8386019da
Butholenkosi Moyo <44831936+mtallbert@users.noreply.github.com>
2018-12-12 14:22:15 +0200

    Add files via upload

---
+++ assignment 1.11476.r
@@ -0,0 +1,19 @@
+# Question 1a: TRUE or FALSE? Prescriptive analytics i used to perdict the future Outcomes
+
+# Answer: FALSE
+
+# Question 1b: BASE R packages are installed automatically
+
+# Answer: TRUE
+
+# Question 2: What is Recycling of elements in a vector?
+
+# Answer: Recycling elements of a vector is simple reeating the elements of a shorter vector until it is long enough to match the longer vector.
+#         This is usually useful when applying an operation to two vectors that requires them to be the same length like addition.
+
+# Question 3. Give an example of recycling of elements.
+
+# Answer:
+
+c(2,3,4,6,7,9) + c(3,2,5)
+
